74b5d0bde249cf940ade17b3efdc4fd8a8b0a7af
Lutfan Davi <lutfan04@gmail.com>
2023-09-18 19:27:22 +0700

    add view binding in main activity

--- app/src/main/java/org/d3if0099/mobpro2/MainActivity.kt
+++ app/src/main/java/org/d3if0099/mobpro2/MainActivity.kt
@@ -2,10 +2,16 @@ package org.d3if0099.mobpro2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if0099.mobpro2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        }
 }
